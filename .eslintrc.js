fb9aa9665043bb0857a6e227fb722897265e048f
Nick Rameau <r@meau.me>
2021-05-27 14:57:55 -0400

    Ignore specific rules globally

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,15 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    // If you don't build your index.js file yet,
+    // you'll get an import error without disabling those two
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    // The whole program involves logging something in the terminal
+    // So we need to disable this rule
+    'no-console': 0,
+    // This allows us to use undefined variables in our Jest tests
+    // Stuffs like 'describe' and 'expect'
+    'no-undef': 0,
   },
 };
