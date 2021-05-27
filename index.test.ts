14e71a3bbc3d2f0f9f10c15f6007faedcddd0ecd
Nick Rameau <r@meau.me>
2021-05-27 13:00:53 -0400

    Fix code structure and disable no-console rule

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 import sayHiNPM from './index'
 
 describe('sayHiNPM', () => {
   test('should log "hi"', () => {
+    // eslint-disable-next-line no-console
     console.log = jest.fn();
 
     sayHiNPM();
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 export default function sayHiNPM(): void {
+  // eslint-disable-next-line no-console
   console.log('hi');
 }
 
-if (require.main === module)
-  sayHiNPM();
+if (require.main === module) sayHiNPM();
